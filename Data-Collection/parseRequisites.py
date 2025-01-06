266881555722d7ad52b6654f0811873804bb9bac
TheRad1 <landon.bakken@gmail.com>
2025-01-05 23:09:41 -0600

    prohibited work now, and a few more formatting things fixed

--- Data-Collection/parseRequisites.py
+++ Data-Collection/parseRequisites.py
@@ -1,21 +1,29 @@
 import re
 
 testRequisites = [
-    #don't work:
+	#don't work:
 	#"PHM PRAC 656 or concurrent enrollment",
 	#"Nursing BSN (Traditional, Accelerated or Collaborative), senior standing and declared in Health Promotion and Health Equity BS, or graduate/professional standing",
 	#"(ACCT I S 340 or concurrent enrollment), (ACCT I S 100 and GEN BUS 307 or 317), (ACCT I S 700 or concurrent enrollment), or declared in the Business Exchange program",
-	#"Not open to students declared in the Nursing, Physician Assistant, or Doctor of Pharmacy programs",
 	#"(CHEM 103, 109, or 115) and (MATH 112, 114, 171 or placement into MATH 221), or graduate/professional standing"
 	
 	#work:
+	#"MATH 96 or placement into MATH 112 or satisfied Quantitative Reasoning (QR) A requirement. Not open to students with credit for ECON 101, 102, or 111"
+	#"Not open to students declared in the Nursing, Physician Assistant, or Doctor of Pharmacy programs",
 	#"PHM PRAC 554, 556 and PHMCOL-M/PHM SCI  522",
 	#"PHM PRAC 550, 555, 556, and 655",
 	#"PHM PRAC 653, 655 and PHM SCI 623",
 	#"Declared in Doctor of Pharmacy program with second year standing",
 	#"Declared in MS Pharmaceutical Sciences: Psychoactive Pharmaceutical Investigation or Capstone Certificate in Psychoactive Pharmaceutical Investigation",
 	#"PHM PRAC 655 or (NURSING 312 and NURSING 422)",
-]	
+]
+"""
+things that don't work:
+when there is an operator right after () in a list
+current enrolment
+lists where they arent classes (just need to figure out how to get rid of them)
+"""
+
 
 operators = [" or ", " and ", "("]
 
@@ -44,10 +52,10 @@ def splitByParenthesis(string):
 	return [x for x in thing if x != '']
 
 def findNumberIndex(string):
-    match = re.search(r'\d', string)
-    if match:
-        return match.start()
-    return -1  # Return -1 if no number is found
+	match = re.search(r'\d', string)
+	if match:
+		return match.start()
+	return -1  # Return -1 if no number is found
 
 #takes string such as "COMP SCI 100, 200, and 300" and converts to 
 def combinedCoursesToList(combinedCourse):
@@ -107,7 +115,7 @@ def splitByCommas(requisites):
 		
 
 #takes string, returns list of requisites (through the layers)
-def loopThroughLayers(requisites, operators):
+def splitByOperators(requisites, operators):
 	requisites = requisites.strip()
 	requisiteList = []
 	
@@ -134,7 +142,7 @@ def loopThroughLayers(requisites, operators):
 		for requisite in requisitesSplit:
 			if requisite not in operators:
 				
-				nextLayer = loopThroughLayers(requisite, operators)
+				nextLayer = splitByOperators(requisite, operators)
 				
 				#get rid of layers that have only one element
 				if len(nextLayer) == 1:
@@ -143,20 +151,43 @@ def loopThroughLayers(requisites, operators):
 					requisiteList.append(nextLayer)
 			
 	return requisiteList
-			
+
+def seperateProhibited(requisites):
+	#going off of assumptions (from looking at the data):
+	#always the last part of a requisite string, so you can just split it
+	#also always "Not" (note the capital N)
+	
+	splitRequisites = requisites.split("Not")
+	
+	#no prohibited
+	if len(splitRequisites) == 1:
+		return requisites, None
+	#prohibited
+	elif len(splitRequisites) == 2:
+		return splitRequisites[0], "Not" + splitRequisites[1]
+	#idk
+	else:
+		print("OH NO!!!!!!!!!!!!!") #I need sleep
+		return None, None
+		
 
 def parseRequisites(requisiteString, operators):
+	#seperate prohibited
+	requisiteString, prohibited = seperateProhibited(requisiteString)
+	
 	#get rid of space in between comma and operator. makes them not get seperated initially
 	for operator in ["and ", "or "]:
 		requisiteString = requisiteString.replace(", " + operator, "," + operator)
 	
-	finalRequisites = loopThroughLayers(requisiteString, operators)
+	requisites = splitByOperators(requisiteString, operators)
 	
-	print("After seperating by operators: " + str(finalRequisites) + "\n")
-	finalRequisites = splitByCommas(finalRequisites)
-		
-	return finalRequisites
+	print("After seperating by operators: " + str(requisites) + "\n")
+	requisites = splitByCommas(requisites)
+	
+	return requisites, prohibited
 
 for requisiteString in testRequisites:
 	print(f"\nStart: {requisiteString}\n")
-	print(f"\nFinal: {parseRequisites(requisiteString, operators)}\n")
+	requisites, prohibited = parseRequisites(requisiteString, operators)
+	print(f"Requisites: {requisites}")
+	print(f"Prohibited: {prohibited}")
