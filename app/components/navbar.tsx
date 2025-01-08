f066dda92ce69e9aad513d732276f60b6fb1ab2a
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2025-01-08 15:04:24 -0800

    lkjsdoifjodsijfd

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,3 +1,16 @@
+import { ReactNode } from "react";
+import { MdOutlineFileUpload, MdHistory } from "react-icons/md";
+
+function IconButton({icon, label}: {icon: ReactNode, label: string}) {
+    return <button className="w-min p-1 my-2 text-xs rounded-md flex flex-col justify-center items-center">
+        {icon}
+        <label className="cursor-pointer">{label}</label>
+    </button>
+}
+
 export default function NavBar() {
-    
+    return <nav className="sticky top-0 left-0 h-screen w-16 m-0 shadow-lg bg-slate-50 flex flex-col items-center">
+        <IconButton icon={<MdOutlineFileUpload size="25"/>} label="DARS"/>
+        <IconButton icon={<MdHistory size="25"/>} label="Recents"/>
+    </nav>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactFlow } from '@xyflow/react';
  
 import '@xyflow/react/dist/style.css';
+import SearchBar from './components/searchbar';
  
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -10,10 +11,13 @@ const initialNodes = [
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
  
 export default function App() {
+  
   return (
-    <div>
-    {/* <div style={{ width: '100vw', height: '100vh' }}> */}
-      {/* <ReactFlow nodes={initialNodes} edges={initialEdges} /> */}
+    <div className="w-full flex flex-col">
+      <SearchBar/>
+      <div className="flex-1 border-black">
+        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      </div>
     </div>
   );
 }
