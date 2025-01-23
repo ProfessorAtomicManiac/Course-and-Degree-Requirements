24351978335f48affe283c21a85fdb3c734627ad
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2025-01-22 22:24:52 -0600

    Fixed Issue #1 - Search bar no longer shrinks

--- components/search/searchComp.tsx
+++ components/search/searchComp.tsx
@@ -6,10 +6,12 @@ import { SearchContext } from "./searchContext";
 export default function SearchComponent() {
 
     const {searchedCourse} = useContext(SearchContext);
-    return searchedCourse !== null ? <section className="absolute w-1/3 h-screen bg-background border-r shadow-lg">
-        <SearchBar />
-        <SearchExpand course={searchedCourse}/></section> : 
-        <div className="absolute w-1/3">
+    return searchedCourse !== null ? 
+        <section className="box-content absolute w-1/3 h-screen bg-background border-r shadow-lg flex flex-col">
+          <SearchBar />
+          <SearchExpand course={searchedCourse}/>
+        </section> 
+        : <section className="box-content absolute w-1/3">
             <SearchBar />
-        </div>
+        </section>
 }
