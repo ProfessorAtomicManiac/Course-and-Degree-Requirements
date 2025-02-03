a82d7b8063ae7c810e0c944fca722fa3cb6e9853
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2025-02-03 10:26:37 -0600

    changing handles to left and right for node

--- components/courseMap/courseNode.tsx
+++ components/courseMap/courseNode.tsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react"
 import Course, { NUT9000 } from '../../types/course.ts'
 import { SearchContext } from "../search/searchContext.tsx";
 
+// implement NodeProps<CourseNode> to handle "handles", "edges", "labels", etc
 export default function CourseNode({ data }: { data: Course }) {
 
     const [hasTaken, _] = useState(false);
@@ -14,11 +15,11 @@ export default function CourseNode({ data }: { data: Course }) {
 
     return(
         <div className={"h-8 w-32 border-2 border-gray-500 rounded-full flex justify-center items-center cursor-pointer " + (hasTaken ? "bg-green-400" : "bg-red-400")} onClick={handleClick}>
-            <Handle type="target" position={Position.Top}/>
+            <Handle type="target" position={Position.Left}/>
             <div>
                 <span>{data.courseCode}</span>
             </div>
-            <Handle type="source" position={Position.Bottom}/>
+            <Handle type="source" position={Position.Right}/>
         </div>
     )
 }
